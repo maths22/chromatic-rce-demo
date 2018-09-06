dd40de36fb95eb6f8f914e831da4476ae9b6696c
Jacob Burroughs <jburroughs@instructure.com>
2018-09-06 09:51:32 -0500

    Parallelize rce for test run

--- src/stories/index.js
+++ src/stories/index.js
@@ -12,6 +12,34 @@ const rceProps = {
   textareaId: "textarea"
 }
 
-storiesOf('Demo', module).add('Canvas RCE', () => <CanvasRce
+storiesOf('Demo', module)
+.add('Canvas RCE 1', () => <CanvasRce
   rceProps={rceProps}
+/>)
+.add('Canvas RCE 2', () => <CanvasRce
+rceProps={rceProps}
+/>)
+.add('Canvas RCE 3', () => <CanvasRce
+rceProps={rceProps}
+/>)
+.add('Canvas RCE 4', () => <CanvasRce
+rceProps={rceProps}
+/>)
+.add('Canvas RCE 5', () => <CanvasRce
+rceProps={rceProps}
+/>)
+.add('Canvas RCE 6', () => <CanvasRce
+rceProps={rceProps}
+/>)
+.add('Canvas RCE 7', () => <CanvasRce
+rceProps={rceProps}
+/>)
+.add('Canvas RCE 8', () => <CanvasRce
+rceProps={rceProps}
+/>)
+.add('Canvas RCE 9', () => <CanvasRce
+rceProps={rceProps}
+/>)
+.add('Canvas RCE 10', () => <CanvasRce
+rceProps={rceProps}
 />);
